932bf0c2b51354980cbf2394e8ab702c13ead832
Sean Arseneault <itsmesean88@gmail.com>
2020-03-10 09:47:28 -0700

    initial boilerplate

---
+++ client/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// IMPORT CONTAINERS AND COMPONENTS HERE
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>arteMetrics</h1>
+        under construction...
+      </div>
+    );
+  }
+}
+
+export default App;
