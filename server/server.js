4bacd3c48769f9908c7eb97e2c0ff753b6bcea1c
skang1004 <saejin.kang1004@gmail.com>
2020-03-13 16:13:18 -0700

    added dependencies, switched to apollo server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,13 @@
-const path = require('path');
+//Apollo-server
 const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { typeDefs } = require('./schema');
 
 const app = express();
-
-/**
- * REQUIRE IN ROUTERS HERE
- */
-const apiRouter = require('./routes/api');
-
 const PORT = 3000;
-
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 /**
  * Handle parsing of the body and cookies
  */
@@ -19,35 +15,94 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-/**
- *  Route handlers
- */
-app.use('/api', apiRouter);
+//demo schema
+const schema = gql`
+  #need to define our demo graphql schema here
+  type Query {
+    users: [User!]
+    user(id: ID!): User
+    me: User
+  }
 
-// response with main app
-app.get('/', (req, res) =>
-  res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
-);
+  type User {
+    id: ID!
+    username: String!
+  }
+`;
 
-// catch-all route handler for any requests to an unknown route
-app.use((req, res) => res.sendStatus(404));
+//demo users
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch'
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids'
+  }
+};
+const me = users[1];
+//demo resolvers
+const resolvers = {
+  Query: {
+    me: () => {
+      return me;
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    users: () => {
+      return Object.values(users);
+    }
+  }
+};
 
-app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 400,
-    message: { err: 'An error occurred. Check server logs for detials.' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+//defining an instance of the ApolloServer
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  //set the playground to false, need to figure out a way to render our index.html
+  playground: false
 });
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 process.env.NODE_ENV = 'development';
 console.log(process.env);
-/**
- * Start server
- */
+
 app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
+  console.log(`Server is listening on http://localhost:3000/graphql`);
 });
+
+// /**
+//  * REQUIRE IN ROUTERS HERE
+//  */
+// const apiRouter = require('./routes/api');
+
+// /**
+//  *  Route handlers
+//  */
+// app.use('/api', apiRouter);
+
+// // response with main app
+// app.get('/', (req, res) =>
+//   res.status(200).sendFile(path.resolve(__dirname, '../index.html'))
+// );
+
+// // catch-all route handler for any requests to an unknown route
+// app.use((req, res) => res.sendStatus(404));
+
+// app.use((err, req, res, next) => {
+//   const defaultErr = {
+//     log: 'Express error handler caught unknown middleware error',
+//     status: 400,
+//     message: { err: 'An error occurred. Check server logs for detials.' }
+//   };
+//   const errorObj = Object.assign({}, defaultErr, err);
+//   console.log(errorObj.log);
+//   return res.status(errorObj.status).json(errorObj.message);
+// });
+
+// /**
+//  * Start server
+//  */
