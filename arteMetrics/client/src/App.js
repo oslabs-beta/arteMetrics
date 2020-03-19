e4a90019b7ade61218aa42426bf66cd39df8dd69
Brian Chiang <chiangbri@gmail.com>
2020-03-19 15:48:33 -0700

    app is loading particles for landing page

--- arteMetrics/client/src/App.js
+++ arteMetrics/client/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
 import ApolloClient from 'apollo-boost';
+import Particles from 'react-particles-js';
 import { ApolloProvider } from 'react-apollo';
+import 'animate.css/animate.min.css';
+import './styles/styles.css';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000'
@@ -11,7 +14,31 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <h1>arteMetrics under construction...</h1>
+        <Particles
+          className="landing-bg"
+          params={{
+            particles: {
+              number: {
+                value: 200
+              },
+              size: {
+                value: 3
+              }
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'repulse'
+                }
+              }
+            }
+          }}
+        />
+        <h1>Hello World</h1>
+        {/* <TopNavbar />
+      <Hero />
+      <Features /> */}
       </div>
     </ApolloProvider>
   );
