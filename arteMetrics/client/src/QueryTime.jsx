a1e16b26557e7d686a01fc30c4d1fbeff3b8c93c
Noah King <king@Noahs-MacBook-Pro.local>
2020-03-19 17:15:37 -0700

    rendered the visualization chart however all the data isnt being displayed. starting on that next

---
+++ arteMetrics/client/src/QueryTime.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import Data from './querydata.json';
+
+// console.log(Data)
+
+const QueryTime = () => {
+  const results = [];
+  const labels = [];
+
+  for (let i in Data) {
+    results.push(Data[i].duration.toString());
+    labels.push(i.toString());
+  }
+  // console.log(results);
+
+  const [queryData, setQueryData] = useState({});
+
+  const chart = () => {
+    setQueryData({
+      labels: labels,
+      datasets: [
+        {
+          data: results
+        }
+      ]
+    });
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+
+  return (
+    <div>
+      <Bar data={queryData} />
+    </div>
+  );
+};
+
+export default QueryTime;
