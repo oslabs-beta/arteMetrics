f4e031139196335eea812530fe3cfa2270b2bd4d
Noah King <king@Noahs-MacBook-Pro.local>
2020-03-19 18:21:31 -0700

    correcting a small syntax error

--- arteMetrics/client/src/components/MainContainer.jsx
+++ arteMetrics/client/src/components/MainContainer.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-// import NavBar from './components/NavBar';
 import QueryTime from './QueryTime.jsx';
 
 const MainContainer = () => {
   return (
     <div>
-      {/* <NavBar /> */}
       <div>
         <QueryTime />
       </div>
