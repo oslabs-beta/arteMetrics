280b77c4805dd1c7e0b1034e87e7bea4ea20bb23
Joseph Renolayan <jrenolayan@gmail.com>
2020-03-20 11:55:09 -0700

    updated middleware to getName and query tracing info + user api key

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+require("dotenv").config();
 const arteMetrics = {};
 const db = require("./db/model");
 
+let name;
+
+arteMetrics.getName = query => {
+  name = query.definitions[0].name.value;
+};
+
 arteMetrics.process = response => {
+  const {
+    startTime,
+    endTime,
+    duration,
+    execution
+  } = response.extensions.tracing;
   const text =
-    "INSERT INTO trace_response(fieldName, startOffset, duration) VALUES($1, $2, $3) RETURNING *";
+    "INSERT INTO queries(api_key, name, start_time, end_time, duration, resolvers) VALUES($1, $2, $3, $4, $5, $6) RETURNING *";
   const values = [
-    response.extensions.tracing.execution.resolvers[0].fieldName,
-    response.extensions.tracing.execution.resolvers[0].startOffset,
-    response.extensions.tracing.execution.resolvers[0].duration
+    process.env.API_KEY,
+    name,
+    startTime,
+    endTime,
+    duration,
+    JSON.stringify(execution.resolvers)
   ];
 
   db.query(text, values)
