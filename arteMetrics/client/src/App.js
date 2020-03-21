ec4697c8c6cf2beeec02e4c4f5ee356b1f28318c
Brian Chiang <chiangbri@gmail.com>
2020-03-20 18:17:56 -0700

    styling changes to maincontainer. the container is no longer pushed way to the bottom of the page.

--- arteMetrics/client/src/App.js
+++ arteMetrics/client/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
-import MainContainer from './components/MainContainer.jsx';
 import 'animate.css/animate.min.css';
 import './styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Home from './components/Home.jsx'
+import TopNavBar from './components/TopNavBar.jsx';
+import MainContainer from './components/MainContainer.jsx';
 
-
+import Home from './components/Home.jsx';
 
 function App() {
   return (
-  <div>
-    <Router>
-    <Route path="/" exact component={Home}/>
-    <Route path="/home" component={Home}/>
-    <Route path="/metrics" component={MainContainer} />
-    </Router>
-  </div>
+    <div>
+      <TopNavBar />
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/metrics" component={MainContainer} />
+      </Router>
+    </div>
   );
 }
 
--- arteMetrics/client/src/components/Home.jsx
+++ arteMetrics/client/src/components/Home.jsx
@@ -3,7 +3,6 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import Features from './Features.jsx';
 import Hero from './Hero.jsx';
-import TopNavBar from './TopNavBar.jsx';
 import 'animate.css/animate.min.css';
 import '../styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,7 +12,6 @@ const client = new ApolloClient({
   uri: 'http://localhost:3000'
 });
 //just adding a comment
-
 
 const Home = () => {
   return (
@@ -41,9 +39,8 @@ const Home = () => {
             }
           }}
         />
-          <TopNavBar />
-          <Hero />
-          <Features />
+        <Hero />
+        <Features />
       </div>
     </ApolloProvider>
   );
