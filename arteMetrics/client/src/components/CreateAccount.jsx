c2418fc3b0674f4ee1f32519c5248559573353d8
Brian Chiang <chiangbri@gmail.com>
2020-03-23 13:48:42 -0700

    setup initial create account functionality with createAccount button. not functional but merging in Joe's recent PR

--- arteMetrics/client/src/components/CreateAccount.jsx
+++ arteMetrics/client/src/components/CreateAccount.jsx
@@ -1,43 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 const CreateAccount = () => {
+  const [count, setCount] = useState(0);
+  const [post, setPost] = useState(false);
+
+  function fetchGQL() {
+    console.log('inside fetchGQL');
+    fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        hi: 'bye'
+      })
+    })
+      .then(data => data.json())
+      .then(myJson => console.log(myJson));
+  }
+
   return (
     <div class="loginpanel">
-      <form>
-        <label for="username" translate-context="Label" translate></label>
-        <input
-          type="text"
-          id="loginUsername"
-          name="username"
-          required
-          placeholder="Username"
-        />
-        <br />
-        <label for="password" translate-context="Password" translate></label>
-        <input
-          type="password"
-          id="loginPassword"
-          name="password"
-          required
-          placeholder="Password"
-        />
-        <label
-          for="confirmPassword"
-          translate-context="Password"
-          translate
-        ></label>
-        <input
-          type="password"
-          id="loginPassword"
-          name="password"
-          required
-          placeholder="Confirm Password"
-        />
-        <br />
-        <input type="submit" id="loginSubmitButton" value="Create Account" />
-      </form>
+      {/* <form method="post"> */}
+      <label for="username" translate-context="Label" translate></label>
+      <input
+        type="text"
+        id="loginUsername"
+        name="username"
+        required
+        placeholder="Username"
+      />
+      <br />
+      <label for="password" translate-context="Password" translate></label>
+      <input
+        type="password"
+        id="loginPassword"
+        name="password"
+        required
+        placeholder="Password"
+      />
+      <label
+        for="confirmPassword"
+        translate-context="Password"
+        translate
+      ></label>
+      <input
+        type="password"
+        id="loginPassword"
+        name="password"
+        required
+        placeholder="Confirm Password"
+      />
+      <br />
+      <input
+        type="submit"
+        id="loginSubmitButton"
+        value="Create Account"
+        // onClick={() => setCount(count + 1)}
+        onClick={() => fetchGQL()}
+      />
+      <p>current Count: {count}</p>
+      {/* </form> */}
     </div>
   );
 };
