aaa123822df78448d55f5fada6e176d4034e23ea
Sean Arseneault <itsmesean88@gmail.com>
2020-03-24 15:55:16 -0700

    implemented tabbing and bar graph

--- arteMetrics/client/src/components/MainContainer.jsx
+++ arteMetrics/client/src/components/MainContainer.jsx
@@ -1,13 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { Tabs, useTabState, Panel } from '@bumaga/tabs';
 import QueryTime from './QueryTime.jsx';
+import Bar from './Bar.jsx';
 import TopNavBar from './TopNavBar.jsx';
 
-const MainContainer = () => {
+const cn = (...args) => args.filter(Boolean).join(' ');
+
+const Tab = ({ children }) => {
+  const { isActive, onClick } = useTabState();
+
   return (
-    <section id="chart">
-      <QueryTime />
-    </section>
+    <button className={cn('tab', isActive && 'active')} onClick={onClick}>
+      {children}
+    </button>
   );
 };
 
-export default MainContainer;
+export default () => (
+  <Tabs>
+    <div className="tabs">
+      <div className="tab-list">
+        <Tab>Tracing</Tab>
+        <Tab>Bar Graph</Tab>
+        <Tab>More Tracing</Tab>
+      </div>
+      <div className="tab-progress" />
+      <Panel>
+        <div>
+          <QueryTime id="chart" />
+        </div>
+      </Panel>
+      <Panel>
+        <div>
+          <Bar id="chart" />
+        </div>
+      </Panel>
+      <Panel>
+        <p>other graph</p>
+      </Panel>
+    </div>
+  </Tabs>
+);
