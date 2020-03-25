72187f571da51b0a69c5fecbd3887964ac3ad518
Joseph Renolayan <jrenolayan@gmail.com>
2020-03-24 21:49:01 -0700

    deleted 'server' folder and added query data logic (daily top 5)

--- arteMetrics/client/src/components/QueriesOverview.jsx
+++ arteMetrics/client/src/components/QueriesOverview.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const QueriesOverview = () => {
-  fetch('/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      // query for dummy apikey account
-      query: `query {
+  const [topFive, setTopFive] = useState([]);
+
+  useEffect(() => {
+    fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        // query for dummy apikey account
+        query: `query {
           allQueries {
             id
             name
@@ -17,16 +20,77 @@ const QueriesOverview = () => {
             end_time
           }
         }`
+      })
     })
-  })
-    .then(data => data.json())
-    .then(myJson => console.log('data back: ', myJson))
-    .catch(err => console.log(err));
+      .then(data => data.json())
+      .then(myJson => {
+        // queries array
+        const data = myJson.data.allQueries;
+        console.log('data back: ', data);
+
+        // filter the results to only show top 5 ('frequent' state)
+        const names = data.map(item => item.name);
+        const counter = {};
+        for (let i = 0; i < names.length; i++) {
+          if (counter.hasOwnProperty(names[i])) {
+            counter[names[i]] += 1;
+          } else counter[names[i]] = 1;
+        }
+        const sortable = [];
+        for (let names in counter) {
+          sortable.push([names, counter[names]]);
+        }
+        const result = sortable.sort((a, b) => b[1] - a[1]);
+        const topFive = result.slice(0, 5);
+
+        const topObj = [];
+
+        // grab latest objects based on top5
+        for (let i = 0; i < topFive.length; i++) {
+          let top = topFive[i][0];
+
+          // get the array of the same query name
+          let topArray = [];
+          for (let item in data) {
+            if (data[item].name === top) {
+              topArray.push(data[item]);
+            }
+          }
+          // grab the latest date of that query
+          for (let i in topArray) {
+            let d1 = new Date(topArray[i].start_time);
+            let d2 = new Date(
+              Math.max.apply(
+                null,
+                topArray.map(item => {
+                  return new Date(item.start_time);
+                })
+              )
+            );
+            if (d1.getTime() === d2.getTime()) {
+              topObj.push(topArray[i]);
+            }
+          }
+        }
+        setTopFive(topObj);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div class="queriesOverview">
       <h1>Queries Overview</h1>
+      <h3>Requests By Frequency</h3>
+      <ul>
+        {topFive.map((item, i) => (
+          <li key={`top_${[i]}`}>
+            <a href={`/${item.id}`}>{item.name}</a>.......................
+            {Math.floor(item.duration / 1000000)}ms
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
+
 export default QueriesOverview;
---
+++ arteMetrics/client/src/components/datalogic.js
@@ -0,0 +1,772 @@
+const data = [
+  {
+    id: 417,
+    name: 'IntrospectionQuery',
+    duration: 40129179,
+    start_time: '2020-03-25T05:55:40.099Z',
+    end_time: '2020-03-25T05:55:40.139Z'
+  },
+  {
+    id: 418,
+    name: 'IntrospectionQuery',
+    duration: 12390084,
+    start_time: '2020-03-25T05:55:40.915Z',
+    end_time: '2020-03-25T05:55:40.927Z'
+  },
+  {
+    id: 419,
+    name: 'IntrospectionQuery',
+    duration: 26424553,
+    start_time: '2020-03-25T05:55:42.246Z',
+    end_time: '2020-03-25T05:55:42.273Z'
+  },
+  {
+    id: 420,
+    name: 'IntrospectionQuery',
+    duration: 4895678,
+    start_time: '2020-03-25T05:55:44.307Z',
+    end_time: '2020-03-25T05:55:44.312Z'
+  },
+  {
+    id: 434,
+    name: 'IntrospectionQuery',
+    duration: 8804695,
+    start_time: '2020-03-25T05:58:00.692Z',
+    end_time: '2020-03-25T05:58:00.701Z'
+  },
+  {
+    id: 346,
+    name: 'getLaunchById',
+    duration: 734335137,
+    start_time: '2020-03-24T09:03:16.352Z',
+    end_time: '2020-03-24T09:03:17.086Z'
+  },
+  {
+    id: 347,
+    name: 'getLaunchById',
+    duration: 393455077,
+    start_time: '2020-03-25T05:51:51.986Z',
+    end_time: '2020-03-25T05:51:52.379Z'
+  },
+  {
+    id: 348,
+    name: 'getLaunchById',
+    duration: 126376872,
+    start_time: '2020-03-25T05:51:53.348Z',
+    end_time: '2020-03-25T05:51:53.475Z'
+  },
+  {
+    id: 349,
+    name: 'getLaunchById',
+    duration: 188200413,
+    start_time: '2020-03-25T05:51:54.057Z',
+    end_time: '2020-03-25T05:51:54.245Z'
+  },
+  {
+    id: 350,
+    name: 'getLaunchById',
+    duration: 132492350,
+    start_time: '2020-03-25T05:51:54.435Z',
+    end_time: '2020-03-25T05:51:54.568Z'
+  },
+  {
+    id: 351,
+    name: 'getLaunchById',
+    duration: 106382457,
+    start_time: '2020-03-25T05:51:54.723Z',
+    end_time: '2020-03-25T05:51:54.829Z'
+  },
+  {
+    id: 352,
+    name: 'getLaunchById',
+    duration: 176778897,
+    start_time: '2020-03-25T05:51:54.996Z',
+    end_time: '2020-03-25T05:51:55.173Z'
+  },
+  {
+    id: 353,
+    name: 'getLaunchById',
+    duration: 279234540,
+    start_time: '2020-03-25T05:51:55.294Z',
+    end_time: '2020-03-25T05:51:55.573Z'
+  },
+  {
+    id: 354,
+    name: 'getLaunchById',
+    duration: 156785129,
+    start_time: '2020-03-25T05:51:55.836Z',
+    end_time: '2020-03-25T05:51:55.993Z'
+  },
+  {
+    id: 355,
+    name: 'getLaunchById',
+    duration: 407290014,
+    start_time: '2020-03-25T05:51:55.552Z',
+    end_time: '2020-03-25T05:51:55.960Z'
+  },
+  {
+    id: 356,
+    name: 'getLaunchById',
+    duration: 135940460,
+    start_time: '2020-03-25T05:51:56.583Z',
+    end_time: '2020-03-25T05:51:56.719Z'
+  },
+  {
+    id: 357,
+    name: 'getLaunchById',
+    duration: 131848300,
+    start_time: '2020-03-25T05:51:57.082Z',
+    end_time: '2020-03-25T05:51:57.214Z'
+  },
+  {
+    id: 358,
+    name: 'getLaunchById',
+    duration: 251772905,
+    start_time: '2020-03-25T05:51:57.630Z',
+    end_time: '2020-03-25T05:51:57.881Z'
+  },
+  {
+    id: 359,
+    name: 'sdsdsdsddsds',
+    duration: 243631133,
+    start_time: '2020-03-25T05:52:01.979Z',
+    end_time: '2020-03-25T05:52:02.222Z'
+  },
+  {
+    id: 360,
+    name: 'sdsdsdsddsds',
+    duration: 123083868,
+    start_time: '2020-03-25T05:52:02.662Z',
+    end_time: '2020-03-25T05:52:02.785Z'
+  },
+  {
+    id: 361,
+    name: 'sdsdsdsddsds',
+    duration: 147293726,
+    start_time: '2020-03-25T05:52:02.937Z',
+    end_time: '2020-03-25T05:52:03.084Z'
+  },
+  {
+    id: 362,
+    name: 'anotherTestQuery',
+    duration: 123059232,
+    start_time: '2020-03-25T05:52:09.922Z',
+    end_time: '2020-03-25T05:52:10.045Z'
+  },
+  {
+    id: 363,
+    name: 'anotherTestQuery',
+    duration: 129809459,
+    start_time: '2020-03-25T05:52:10.609Z',
+    end_time: '2020-03-25T05:52:10.739Z'
+  },
+  {
+    id: 364,
+    name: 'anotherTestQuery',
+    duration: 425459340,
+    start_time: '2020-03-25T05:52:11.223Z',
+    end_time: '2020-03-25T05:52:11.649Z'
+  },
+  {
+    id: 365,
+    name: 'anotherTestQuery',
+    duration: 390554855,
+    start_time: '2020-03-25T05:52:11.796Z',
+    end_time: '2020-03-25T05:52:12.186Z'
+  },
+  {
+    id: 366,
+    name: 'anotherTestQuery',
+    duration: 143439006,
+    start_time: '2020-03-25T05:52:12.548Z',
+    end_time: '2020-03-25T05:52:12.692Z'
+  },
+  {
+    id: 367,
+    name: 'anotherTestQuery',
+    duration: 142347640,
+    start_time: '2020-03-25T05:52:13.185Z',
+    end_time: '2020-03-25T05:52:13.327Z'
+  },
+  {
+    id: 368,
+    name: 'HowAboutThis',
+    duration: 949551842,
+    start_time: '2020-03-25T05:52:18.609Z',
+    end_time: '2020-03-25T05:52:19.558Z'
+  },
+  {
+    id: 369,
+    name: 'HowAboutThis',
+    duration: 121798343,
+    start_time: '2020-03-25T05:52:22.487Z',
+    end_time: '2020-03-25T05:52:22.609Z'
+  },
+  {
+    id: 370,
+    name: 'HowAboutThis',
+    duration: 1131053203,
+    start_time: '2020-03-25T05:52:23.839Z',
+    end_time: '2020-03-25T05:52:24.970Z'
+  },
+  {
+    id: 371,
+    name: 'HowAboutThis',
+    duration: 120230719,
+    start_time: '2020-03-25T05:52:25.457Z',
+    end_time: '2020-03-25T05:52:25.577Z'
+  },
+  {
+    id: 372,
+    name: 'HowAboutThis',
+    duration: 148883376,
+    start_time: '2020-03-25T05:52:26.134Z',
+    end_time: '2020-03-25T05:52:26.283Z'
+  },
+  {
+    id: 373,
+    name: 'HowAboutThis',
+    duration: 147617321,
+    start_time: '2020-03-25T05:52:26.721Z',
+    end_time: '2020-03-25T05:52:26.868Z'
+  },
+  {
+    id: 374,
+    name: 'HowAboutThis',
+    duration: 121998607,
+    start_time: '2020-03-25T05:52:27.254Z',
+    end_time: '2020-03-25T05:52:27.376Z'
+  },
+  {
+    id: 375,
+    name: 'HowAboutThis',
+    duration: 177664214,
+    start_time: '2020-03-25T05:52:33.224Z',
+    end_time: '2020-03-25T05:52:33.402Z'
+  },
+  {
+    id: 376,
+    name: 'HowAboutThis',
+    duration: 134539999,
+    start_time: '2020-03-25T05:52:33.566Z',
+    end_time: '2020-03-25T05:52:33.701Z'
+  },
+  {
+    id: 377,
+    name: 'HowAboutThis',
+    duration: 106599478,
+    start_time: '2020-03-25T05:52:33.843Z',
+    end_time: '2020-03-25T05:52:33.950Z'
+  },
+  {
+    id: 378,
+    name: 'HowAboutThis',
+    duration: 337845256,
+    start_time: '2020-03-25T05:52:34.054Z',
+    end_time: '2020-03-25T05:52:34.391Z'
+  },
+  {
+    id: 379,
+    name: 'AndThis',
+    duration: 112861166,
+    start_time: '2020-03-25T05:52:39.950Z',
+    end_time: '2020-03-25T05:52:40.063Z'
+  },
+  {
+    id: 380,
+    name: 'AndThis',
+    duration: 156486066,
+    start_time: '2020-03-25T05:52:40.752Z',
+    end_time: '2020-03-25T05:52:40.908Z'
+  },
+  {
+    id: 381,
+    name: 'AndThis',
+    duration: 132146072,
+    start_time: '2020-03-25T05:52:41.349Z',
+    end_time: '2020-03-25T05:52:41.482Z'
+  },
+  {
+    id: 382,
+    name: 'AndThis',
+    duration: 348291258,
+    start_time: '2020-03-25T05:52:41.933Z',
+    end_time: '2020-03-25T05:52:42.282Z'
+  },
+  {
+    id: 383,
+    name: 'AndThis',
+    duration: 112810109,
+    start_time: '2020-03-25T05:52:42.432Z',
+    end_time: '2020-03-25T05:52:42.545Z'
+  },
+  {
+    id: 384,
+    name: 'AndThis',
+    duration: 132297628,
+    start_time: '2020-03-25T05:52:42.917Z',
+    end_time: '2020-03-25T05:52:43.049Z'
+  },
+  {
+    id: 385,
+    name: 'AndThis',
+    duration: 171745593,
+    start_time: '2020-03-25T05:53:08.669Z',
+    end_time: '2020-03-25T05:53:08.841Z'
+  },
+  {
+    id: 386,
+    name: 'AndThis',
+    duration: 169274308,
+    start_time: '2020-03-25T05:53:09.005Z',
+    end_time: '2020-03-25T05:53:09.175Z'
+  },
+  {
+    id: 387,
+    name: 'AndThis',
+    duration: 144387586,
+    start_time: '2020-03-25T05:53:09.316Z',
+    end_time: '2020-03-25T05:53:09.460Z'
+  },
+  {
+    id: 388,
+    name: 'AndThis',
+    duration: 117578597,
+    start_time: '2020-03-25T05:53:09.628Z',
+    end_time: '2020-03-25T05:53:09.746Z'
+  },
+  {
+    id: 389,
+    name: 'AndThis',
+    duration: 120906274,
+    start_time: '2020-03-25T05:53:09.909Z',
+    end_time: '2020-03-25T05:53:10.030Z'
+  },
+  {
+    id: 390,
+    name: 'Yeah',
+    duration: 220623229,
+    start_time: '2020-03-25T05:53:27.493Z',
+    end_time: '2020-03-25T05:53:27.714Z'
+  },
+  {
+    id: 391,
+    name: 'Yeah',
+    duration: 123269863,
+    start_time: '2020-03-25T05:53:27.798Z',
+    end_time: '2020-03-25T05:53:27.922Z'
+  },
+  {
+    id: 392,
+    name: 'Yeah',
+    duration: 107109886,
+    start_time: '2020-03-25T05:53:28.069Z',
+    end_time: '2020-03-25T05:53:28.176Z'
+  },
+  {
+    id: 393,
+    name: 'Yeah',
+    duration: 182565682,
+    start_time: '2020-03-25T05:53:27.173Z',
+    end_time: '2020-03-25T05:53:27.355Z'
+  },
+  {
+    id: 394,
+    name: 'Yeah',
+    duration: 135379939,
+    start_time: '2020-03-25T05:53:28.540Z',
+    end_time: '2020-03-25T05:53:28.675Z'
+  },
+  {
+    id: 395,
+    name: 'Yeah',
+    duration: 224339224,
+    start_time: '2020-03-25T05:53:28.816Z',
+    end_time: '2020-03-25T05:53:29.041Z'
+  },
+  {
+    id: 396,
+    name: 'Yeah',
+    duration: 132414922,
+    start_time: '2020-03-25T05:53:29.245Z',
+    end_time: '2020-03-25T05:53:29.377Z'
+  },
+  {
+    id: 397,
+    name: 'Yeah',
+    duration: 126299748,
+    start_time: '2020-03-25T05:53:29.625Z',
+    end_time: '2020-03-25T05:53:29.751Z'
+  },
+  {
+    id: 398,
+    name: 'Yeah',
+    duration: 144029441,
+    start_time: '2020-03-25T05:53:29.975Z',
+    end_time: '2020-03-25T05:53:30.119Z'
+  },
+  {
+    id: 399,
+    name: 'Yeah',
+    duration: 157723629,
+    start_time: '2020-03-25T05:53:30.376Z',
+    end_time: '2020-03-25T05:53:30.534Z'
+  },
+  {
+    id: 400,
+    name: 'Yeah',
+    duration: 115974668,
+    start_time: '2020-03-25T05:53:30.736Z',
+    end_time: '2020-03-25T05:53:30.852Z'
+  },
+  {
+    id: 401,
+    name: 'Yeah',
+    duration: 650114156,
+    start_time: '2020-03-25T05:53:30.994Z',
+    end_time: '2020-03-25T05:53:31.644Z'
+  },
+  {
+    id: 402,
+    name: 'Yeah',
+    duration: 213903322,
+    start_time: '2020-03-25T05:53:31.516Z',
+    end_time: '2020-03-25T05:53:31.730Z'
+  },
+  {
+    id: 403,
+    name: 'Yeah',
+    duration: 117121853,
+    start_time: '2020-03-25T05:53:31.869Z',
+    end_time: '2020-03-25T05:53:31.986Z'
+  },
+  {
+    id: 404,
+    name: 'Yeah',
+    duration: 112798657,
+    start_time: '2020-03-25T05:53:32.231Z',
+    end_time: '2020-03-25T05:53:32.344Z'
+  },
+  {
+    id: 405,
+    name: 'getLaunchById',
+    duration: 114049263,
+    start_time: '2020-03-25T05:53:40.433Z',
+    end_time: '2020-03-25T05:53:40.547Z'
+  },
+  {
+    id: 406,
+    name: 'getLaunchById',
+    duration: 201531742,
+    start_time: '2020-03-25T05:53:40.696Z',
+    end_time: '2020-03-25T05:53:40.898Z'
+  },
+  {
+    id: 407,
+    name: 'getLaunchById',
+    duration: 126641510,
+    start_time: '2020-03-25T05:53:40.956Z',
+    end_time: '2020-03-25T05:53:41.083Z'
+  },
+  {
+    id: 408,
+    name: 'getLaunchById',
+    duration: 142394454,
+    start_time: '2020-03-25T05:53:41.187Z',
+    end_time: '2020-03-25T05:53:41.330Z'
+  },
+  {
+    id: 409,
+    name: 'getLaunchById',
+    duration: 140312756,
+    start_time: '2020-03-25T05:53:41.419Z',
+    end_time: '2020-03-25T05:53:41.560Z'
+  },
+  {
+    id: 410,
+    name: 'getLaunchById',
+    duration: 141622763,
+    start_time: '2020-03-25T05:53:41.636Z',
+    end_time: '2020-03-25T05:53:41.777Z'
+  },
+  {
+    id: 411,
+    name: 'getLaunchById',
+    duration: 197346853,
+    start_time: '2020-03-25T05:54:02.875Z',
+    end_time: '2020-03-25T05:54:03.073Z'
+  },
+  {
+    id: 412,
+    name: 'getLaunchById',
+    duration: 364158587,
+    start_time: '2020-03-25T05:54:03.456Z',
+    end_time: '2020-03-25T05:54:03.821Z'
+  },
+  {
+    id: 413,
+    name: 'getLaunchById',
+    duration: 1203141429,
+    start_time: '2020-03-25T05:54:03.964Z',
+    end_time: '2020-03-25T05:54:05.167Z'
+  },
+  {
+    id: 414,
+    name: 'getLaunchById',
+    duration: 198924189,
+    start_time: '2020-03-25T05:54:05.598Z',
+    end_time: '2020-03-25T05:54:05.797Z'
+  },
+  {
+    id: 415,
+    name: 'getLaunchById',
+    duration: 313455814,
+    start_time: '2020-03-25T05:54:06.523Z',
+    end_time: '2020-03-25T05:54:06.836Z'
+  },
+  {
+    id: 416,
+    name: 'getLaunchById',
+    duration: 238305252,
+    start_time: '2020-03-25T05:54:07.347Z',
+    end_time: '2020-03-25T05:54:07.585Z'
+  },
+  {
+    id: 421,
+    name: 'getLaunchById',
+    duration: 425370689,
+    start_time: '2020-03-25T05:55:45.002Z',
+    end_time: '2020-03-25T05:55:45.428Z'
+  },
+  {
+    id: 422,
+    name: 'IntrospectionQuery',
+    duration: 6842805,
+    start_time: '2020-03-25T05:55:46.337Z',
+    end_time: '2020-03-25T05:55:46.344Z'
+  },
+  {
+    id: 423,
+    name: 'IntrospectionQuery',
+    duration: 6554571,
+    start_time: '2020-03-25T05:55:48.366Z',
+    end_time: '2020-03-25T05:55:48.373Z'
+  },
+  {
+    id: 424,
+    name: 'IntrospectionQuery',
+    duration: 13002029,
+    start_time: '2020-03-25T05:55:50.398Z',
+    end_time: '2020-03-25T05:55:50.411Z'
+  },
+  {
+    id: 425,
+    name: 'LaunchDetails',
+    duration: 232851582,
+    start_time: '2020-03-25T05:56:59.344Z',
+    end_time: '2020-03-25T05:56:59.577Z'
+  },
+  {
+    id: 426,
+    name: 'GetLaunchList',
+    duration: 260556949,
+    start_time: '2020-03-25T05:57:04.530Z',
+    end_time: '2020-03-25T05:57:04.791Z'
+  },
+  {
+    id: 427,
+    name: 'LaunchDetails',
+    duration: 199860187,
+    start_time: '2020-03-25T05:57:06.107Z',
+    end_time: '2020-03-25T05:57:06.307Z'
+  },
+  {
+    id: 428,
+    name: 'LaunchDetails',
+    duration: 206673057,
+    start_time: '2020-03-25T05:57:12.072Z',
+    end_time: '2020-03-25T05:57:12.278Z'
+  },
+  {
+    id: 429,
+    name: 'LaunchDetails',
+    duration: 131099500,
+    start_time: '2020-03-25T05:57:13.234Z',
+    end_time: '2020-03-25T05:57:13.365Z'
+  },
+  {
+    id: 430,
+    name: 'BookTrips',
+    duration: 146726053,
+    start_time: '2020-03-25T05:57:15.813Z',
+    end_time: '2020-03-25T05:57:15.960Z'
+  },
+  {
+    id: 431,
+    name: 'GetLaunch',
+    duration: 365037171,
+    start_time: '2020-03-25T05:57:15.994Z',
+    end_time: '2020-03-25T05:57:16.359Z'
+  },
+  {
+    id: 432,
+    name: 'GetMyTrips',
+    duration: 267106106,
+    start_time: '2020-03-25T05:57:17.773Z',
+    end_time: '2020-03-25T05:57:18.040Z'
+  },
+  {
+    id: 433,
+    name: 'LaunchDetails',
+    duration: 124977355,
+    start_time: '2020-03-25T05:57:20.747Z',
+    end_time: '2020-03-25T05:57:20.872Z'
+  },
+  {
+    id: 435,
+    name: 'LaunchDetails',
+    duration: 195709644,
+    start_time: '2020-03-25T05:58:12.275Z',
+    end_time: '2020-03-25T05:58:12.471Z'
+  },
+  {
+    id: 436,
+    name: 'getLaunchById',
+    duration: 470086648,
+    start_time: '2020-03-25T06:29:29.007Z',
+    end_time: '2020-03-25T06:29:29.477Z'
+  },
+  {
+    id: 437,
+    name: 'aNewQuery',
+    duration: 126912893,
+    start_time: '2020-03-25T06:29:38.183Z',
+    end_time: '2020-03-25T06:29:38.310Z'
+  },
+  {
+    id: 438,
+    name: 'aNewQuery',
+    duration: 119381093,
+    start_time: '2020-03-25T06:29:39.732Z',
+    end_time: '2020-03-25T06:29:39.851Z'
+  },
+  {
+    id: 439,
+    name: 'aNewQuery',
+    duration: 431776121,
+    start_time: '2020-03-25T06:29:40.856Z',
+    end_time: '2020-03-25T06:29:41.288Z'
+  },
+  {
+    id: 440,
+    name: 'aNewQuery',
+    duration: 166864644,
+    start_time: '2020-03-25T06:29:45.381Z',
+    end_time: '2020-03-25T06:29:45.548Z'
+  },
+  {
+    id: 441,
+    name: 'aNewQuery',
+    duration: 274974792,
+    start_time: '2020-03-25T07:19:28.773Z',
+    end_time: '2020-03-25T07:19:29.048Z'
+  },
+  {
+    id: 442,
+    name: 'testQ',
+    duration: 193244017,
+    start_time: '2020-03-25T07:19:41.486Z',
+    end_time: '2020-03-25T07:19:41.679Z'
+  },
+  {
+    id: 443,
+    name: 'testQ',
+    duration: 241535645,
+    start_time: '2020-03-25T07:19:42.156Z',
+    end_time: '2020-03-25T07:19:42.398Z'
+  },
+  {
+    id: 444,
+    name: 'testQ',
+    duration: 113678801,
+    start_time: '2020-03-25T07:19:42.798Z',
+    end_time: '2020-03-25T07:19:42.912Z'
+  },
+  {
+    id: 445,
+    name: 'testQ',
+    duration: 368795189,
+    start_time: '2020-03-25T07:19:43.955Z',
+    end_time: '2020-03-25T07:19:44.324Z'
+  },
+  {
+    id: 446,
+    name: 'another',
+    duration: 279400606,
+    start_time: '2020-03-25T07:37:57.833Z',
+    end_time: '2020-03-25T07:37:58.112Z'
+  },
+  {
+    id: 447,
+    name: 'another',
+    duration: 266947092,
+    start_time: '2020-03-25T07:37:58.610Z',
+    end_time: '2020-03-25T07:37:58.877Z'
+  },
+  {
+    id: 448,
+    name: 'another',
+    duration: 168099474,
+    start_time: '2020-03-25T07:37:59.364Z',
+    end_time: '2020-03-25T07:37:59.532Z'
+  }
+];
+
+// top 5 most frequent requests
+const names = data.map(item => item.name);
+const counter = {};
+for (let i = 0; i < names.length; i++) {
+  if (counter.hasOwnProperty(names[i])) {
+    counter[names[i]] += 1;
+  } else counter[names[i]] = 1;
+}
+const sortable = [];
+for (let names in counter) {
+  sortable.push([names, counter[names]]);
+}
+let result = sortable.sort((a, b) => b[1] - a[1]);
+const topFive = result.slice(0, 5);
+
+const topObj = [];
+
+// grab latest objects based on top5
+for (let i = 0; i < topFive.length; i++) {
+  let top = topFive[i][0];
+
+  // get the array of the same query name
+  let topArray = [];
+  for (let item in data) {
+    if (data[item].name === top) {
+      topArray.push(data[item]);
+    }
+  }
+  // grab the latest date of that query
+  for (let i in topArray) {
+    let d1 = new Date(topArray[i].start_time);
+    let d2 = new Date(
+      Math.max.apply(
+        null,
+        topArray.map(item => {
+          return new Date(item.start_time);
+        })
+      )
+    );
+    if (d1.getTime() === d2.getTime()) {
+      topObj.push(topArray[i]);
+    }
+  }
+}
+
+console.log(topObj);
+
+// grab the avg duration of the request
