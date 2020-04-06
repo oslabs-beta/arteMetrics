bf639fd0339180a8301812706e097a5f6dfbcc78
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-06 12:56:00 -0700

    get userinfo based on cookie

--- arteMetrics/client/src/components/containers/CreateAppContainer.jsx
+++ arteMetrics/client/src/components/containers/CreateAppContainer.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 const CreateApp = (props) => {
   const [app, setApp] = useState('');
   const [api, setAPI] = useState('');
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    // fetch current user's name and ID
+    const jwt = Cookies.get('token');
+    fetch('testjwt', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ token: jwt })
+    })
+      .then((data) => data.json())
+      .then((myJson) => {
+        setUser(myJson.user);
+      })
+      .catch((err) => console.log(err));
+  });
 
   function createApp(e) {
     e.preventDefault();
     console.log(app);
     // transaction to add app name and receive an API key to display after 'create'
+
     fetch('/createApp', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        app_name: app
+        app_name: app,
+        user: user
       })
     })
       .then((data) => data.json())
