db55acebbf4176abfc1a0cf742ef49fb58dd622b
Joseph Renolayan <jrenolayan@gmail.com>
2020-04-06 12:54:50 -0700

    added dynamic id into inserting api keys

--- arteMetrics/client/sorver/controllers/appController.js
+++ arteMetrics/client/sorver/controllers/appController.js
@@ -10,15 +10,13 @@ appController.createAPI = (req, res, next) => {
 
   // insert API KEY + APP NAME + USER ID to DB
   const appName = req.body.app_name;
-  console.log(appName);
+  const userName = req.body.user;
+  console.log('App: ' + appName + ' ' + userName);
 
   // make user_id dynamic
-  const text =
-    'INSERT INTO "apiKeys" (user_id, api_key, app_name) VALUES ($1, $2, $3) RETURNING *';
+  const text = `INSERT INTO "apiKeys" (api_key, app_name, user_id) VALUES ('${apiKey}', '${appName}', (SELECT id FROM "users" where username = '${userName}')) RETURNING *`;
 
-  const values = [1, apiKey, appName];
-
-  db.query(text, values)
+  db.query(text)
     .then((res) => {
       console.log(res.rows[0]);
     })
