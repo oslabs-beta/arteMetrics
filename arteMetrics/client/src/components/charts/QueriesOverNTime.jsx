c1cb43ca92972851242f5c1f57fd911141e45b71
Sean Arseneault <itsmesean88@gmail.com>
2020-04-07 15:56:47 -0700

    Removed unused imports, added comments

--- arteMetrics/client/src/components/charts/QueriesOverNTime.jsx
+++ arteMetrics/client/src/components/charts/QueriesOverNTime.jsx
@@ -1,28 +1,30 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { Line } from 'react-chartjs-2';
 import moment from 'moment';
 
-const QueriesOverNTIme = props => {
+const QueriesOverNTIme = (props) => {
   let output = [];
 
   const parse = (data, unit, amount, granularity) => {
+    // filtering out times tht match requested time frame
     let timeFrame = moment().subtract(amount, unit);
     if (data === undefined) return;
-    let temp = data.allQueries.filter(stat =>
+    let temp = data.allQueries.filter((stat) =>
       moment(stat.start_time).isAfter(timeFrame)
     );
 
-    temp = temp.map(item => {
-      return moment(item.start_time)
-        .startOf(granularity)
-        .format('LLL');
+    // rounding down times array to nearest hour/minute depending on granularity
+    temp = temp.map((item) => {
+      return moment(item.start_time).startOf(granularity).format('LLL');
     });
 
-    temp = temp.reduce(function(prev, cur) {
+    // object with counts of queries per hour/minute
+    temp = temp.reduce(function (prev, cur) {
       prev[cur] = (prev[cur] || 0) + 1;
       return prev;
     }, {});
 
+    // chart.js readable data object {x: time, y: count}
     for (let [key, value] of Object.entries(temp)) {
       output.push({ x: key, y: value });
     }
