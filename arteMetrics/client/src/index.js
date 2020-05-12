7f7a94fc7b94b7257d3448fbc2ca45451c2416bb
Brian Chiang <chiangbri@gmail.com>
2020-05-11 21:49:28 -0700

    cleaning up index.js

--- arteMetrics/client/src/index.js
+++ arteMetrics/client/src/index.js
@@ -2,29 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-// const render = () => {
 ReactDOM.render(
-  // <AppContainer>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  // </AppContainer>,
   document.getElementById('root')
 );
-// };
-
-// render();
-
-// if (module.hot) {
-//   module.hot.accept('./App', () => {
-//     render();
-//   });
-// }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
